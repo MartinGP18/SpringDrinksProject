88380a8faea30cc300929c4661c43f1aed90c2a7
MartinGP18 <martingpalmer@hotmail.co.uk>
2022-01-04 11:48:13 +0000

    Initial Commit

---
+++ Spring_Drinks/src/test/java/com/qa/Spring_Drinks/SpringDrinksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.Spring_Drinks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDrinksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
